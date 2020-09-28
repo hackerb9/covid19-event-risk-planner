73ac0b9fa54d549017c81b152de04c3aae3602db
b9 <hackerb9@member.fsf.org>
2020-09-28 14:18:00 -0700

    get rid of state data subdir

--- COVID19-Event-Risk-Planner/update_current.sh
+++ COVID19-Event-Risk-Planner/update_current.sh
@@ -4,4 +4,4 @@ fname=$(date +%Y%m%d_%H%M%S)
 wget https://covidtracking.com/api/v1/states/current.csv \
 	-O "${fname}.csv" \
 	-a "current.log"; 
-[ ! -s "${base}/states_current/${fname}.csv" ] && rm -f "${base}/states_current/${fname}.csv"
+[ ! -s "${fname}.csv" ] && rm -f "${fname}.csv"
